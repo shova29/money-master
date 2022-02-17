487a306ce0f6f62fadd89bf690cf6a253c973b86
shova29 <halima2996@gmail.com>
2022-02-17 23:53:07 +0600

    fourth commit

--- js/calculation.js
+++ js/calculation.js
@@ -2,19 +2,97 @@
 function getInputValue(inputId) {
   let inputField = document.getElementById(inputId);
   let amountValue = parseInt(inputField.value);
-  //   let amountValue;
-  //   checking value is string and negative number or not
-  //   if (isNaN(amountValue) || amountValue < 0) {
-  //     alert("Please enter a valid amount");
-  //   }
-  //   //   checking the value is empty or not
-  //   if (inputField.value != "") {
-  //     amountValue = parseInt(inputField.value);
-  //   } else {
-  //     // inputValue = parseInt(input.value);
-  //     amountValue = 0;
-  //   }
-
   // inputField.value = "";
   return amountValue;
+}
+
+// handle income event handler
+document
+  .getElementById("calculate-button")
+  .addEventListener("click", function () {
+    //get user amount
+    calculateExpenses();
+  });
+
+function calculateExpenses() {
+  let incomeAmount = getInputValue("income-input");
+  let foodCost = getInputValue("food-cost");
+  let rentCost = getInputValue("rent-cost");
+  let clothesCost = getInputValue("clothes-cost");
+
+  //update on the html
+
+  if ((!isNaN(incomeAmount) && incomeAmount > 0) || incomeAmount == "") {
+    document.getElementById("error-message").innerText = "";
+    if ((!isNaN(foodCost) && foodCost >= 0) || foodCost == "") {
+      document.getElementById("error-message").innerText = "";
+      if ((!isNaN(rentCost) && rentCost >= 0) || rentCost == "") {
+        document.getElementById("error-message").innerText = "";
+        if ((!isNaN(clothesCost) && clothesCost >= 0) || clothesCost == "") {
+          document.getElementById("error-message").innerText = "";
+          let totalExpenses = foodCost + rentCost + clothesCost;
+          let balanceAmount = incomeAmount - totalExpenses;
+          if (incomeAmount > totalExpenses) {
+            document.getElementById("total-expenses").innerText = totalExpenses;
+            document.getElementById("balance-amount").innerText = balanceAmount;
+          } else {
+            document.getElementById("total-expenses").innerText =
+              "Oops! Too much spend on expenses rather than income!!!";
+            document.getElementById("balance-amount").innerText =
+              "Dear, you are out of balance";
+          }
+        } else {
+          document.getElementById("error-message").innerText =
+            "Please enter a valid Input";
+        }
+      } else {
+        document.getElementById("error-message").innerText =
+          "Please enter a valid Input";
+      }
+    } else {
+      document.getElementById("error-message").innerText =
+        "Please enter a valid Input";
+    }
+  } else {
+    document.getElementById("error-message").innerText =
+      "Please enter a valid Input";
+  }
+}
+
+// handle isaving event handler
+document.getElementById("save-button").addEventListener("click", function () {
+  //get user amount
+  savingMoney();
+});
+function savingMoney() {
+  let incomeAmount = parseInt(document.getElementById("income-input").value);
+  let savingAmount = parseInt(document.getElementById("saving-input").value);
+  let remainingAmount = parseInt(
+    document.getElementById("balance-amount").innerText
+  );
+  if (!isNaN(remainingAmount)) {
+    if ((!isNaN(savingAmount) && savingAmount >= 0) || savingAmount == "") {
+      // document.getElementById("saving-input").innerText = "";
+      let savingsAmount = (savingAmount / 100) * incomeAmount;
+      let remainingBalance = remainingAmount - savingsAmount;
+      console.log(savingsAmount);
+      if (savingsAmount <= remainingAmount) {
+        document.getElementById("savings-amount").innerText = savingsAmount;
+        document.getElementById("remaining-balance").innerText =
+          remainingBalance;
+      } else {
+        document.getElementById("savings-amount").innerText =
+          "You don't have enough money to save";
+        document.getElementById("remaining-balance").innerText =
+          remainingAmount;
+      }
+    } else {
+      alert("Please enter a valid Input");
+    }
+  } else {
+    document.getElementById("savings-amount").innerText =
+      "You don't have enough money to save";
+    document.getElementById("remaining-balance").innerText =
+      "You have no money";
+  }
 }
